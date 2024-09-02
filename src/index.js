bcca1fadb63787b81fcf2eea564fde029743a371
palakkaushik <kaushik.palak05@gmail.com>
2024-09-02 10:01:55 +0530

    frontend_updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-import reportWebVitals from './reportWebVitals';
+import HomePage from "./landing_page/home/HomePage";
+ import Signup from "./landing_page/signup/Signup";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductsPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ import NotFound from "./landing_page/NotFound";
+ import Navbar from "./Navbar";
+ import Footer from "./Footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-   <div>
-    <h1>hello</h1>
-   </div>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+     <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} /> 
+      <Route path="*" element={<NotFound />} /> 
+    </Routes>
+     <Footer /> 
+  </BrowserRouter>
 );
-
